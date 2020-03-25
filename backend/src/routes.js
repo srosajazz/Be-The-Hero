0d3c859717228515b74686c0db6f53971f3c15e1
Sergio Rosa <scarlosjazz@gmail.com>
2020-03-24 21:53:33 -0400

    backend complete

---
+++ backend/src/routes.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+const routes = express.Router();
+
+routes.post('/sessions', SessionController.create);
+
+//List all information from ongs
+routes.get('/ongs', OngController.index);
+
+//create list registration
+routes.post('/ongs', OngController.create);
+
+routes.get('/profile', ProfileController.index);
+
+//incidents 
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+module.exports = routes;
